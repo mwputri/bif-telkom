8ac497aa072595b02aae4a3311d1182663164d1a
mwputri <mwputrimeiliniahp@gmail.com>
2022-07-01 21:07:20 +0700

    TA

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  KEY: '1a2ddab7df4b247c0a70ce8ad1de0db9',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
